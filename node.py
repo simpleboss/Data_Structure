1f7ec0245355e4100182aaf5a765bfd505144f33
Simple Boss <gichan.lee@threemindev.com>
2020-06-30 17:55:34 +0700

    change method name to get_next_node and set_next_node

--- node.py
+++ node.py
@@ -1,28 +1,28 @@
-class Member_node:
-    def __init__(self, value, link_node=None):
+class MemberNode:
+    def __init__(self, value, next_node=None):
         self.value = value
-        self.link_node = link_node
+        self.next_node = next_node
 
     def get_value(self):
         return self.value
 
-    def get_link_node(self):
-        return self.link_node
+    def get_next_node(self):
+        return self.next_node
 
-    def set_link_node(self, link_node):
-        self.link_node = link_node
+    def set_next_node(self, next_node):
+        self.next_node = next_node
 
 
-father = Member_node("Dr Lee")
-mother = Member_node("MK")
-son1 = Member_node("Gichan")
-son2 = Member_node("Gimoon")
+father = MemberNode("Dr Lee")
+mother = MemberNode("MK")
+son1 = MemberNode("Gichan")
+son2 = MemberNode("Gimoon")
 
-father.set_link_node(mother)
-mother.set_link_node(son1)
-son1.set_link_node(son2)
+father.set_next_node(mother)
+mother.set_next_node(son1)
+son1.set_next_node(son2)
 
-print(father.get_value())
-print(mother.get_link_node())
-print(son2.get_link_node())
+# print(father.get_value())
+# print(mother.get_next_node().get_value())
+# print(son2.get_next_node())
 
