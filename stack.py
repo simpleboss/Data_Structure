f9a355c0433c422c8681a66645fcec1287cb65c0
Simple Boss <gichan.lee@threemindev.com>
2020-07-02 16:53:21 +0700

    In push method, added new_node's next node

--- stack.py
+++ stack.py
@@ -10,15 +10,18 @@ class Stack:
     def push(self, value):
         if not self.is_full():
             new_node = Node(value)
+            new_node.get_next_node(self.top_item)
             self.top_item = new_node
             self.size += 1
+
         else:
             print("Stacks is full")
 
     def pop(self):
         if not self.is_empty():
             self.size -= 1
-            return self.top_item.get_value()
+            return_item = self.top_item
+            return return_item.get_value()
         else:
             print("Stack is empty")
 
@@ -36,6 +39,9 @@ class Stack:
 
 
 stack = Stack(10)
-stack.push("one card")
+stack.push("first card")
+stack.push("second card")
+stack.push("third card")
 print(stack.pop())
+print(stack.peek())
 
